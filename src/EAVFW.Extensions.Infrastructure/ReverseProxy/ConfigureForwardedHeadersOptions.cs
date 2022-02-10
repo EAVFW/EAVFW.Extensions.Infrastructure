a61105f6b9d3d41dde38ec77a648a69f7c6df7d2
Poul Kjeldager Sørensen <pks@delegate.dk>
2022-02-10 13:44:27 +0100

    feat: added ConfigureForwardedHeadersOptions

---
+++ src/EAVFW.Extensions.Infrastructure/ReverseProxy/ConfigureForwardedHeadersOptions.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ConfigureForwardedHeadersOptionsExtensions
+    {
+        public static IServiceCollection ConfigureForwardedHeadersOptions(this IServiceCollection services, IConfiguration configuration,string setting= "ForwardedHeaders:KnownProxies")
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+           {
+               foreach (var proxie in configuration.GetSection(setting).Get<string[]>()
+                   ?.Select(IPAddress.Parse) ?? Array.Empty<IPAddress>())
+                   options.KnownProxies.Add(proxie);
+
+               options.ForwardedHeaders =
+                   ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost;
+           });
+
+            return services;
+
+        }
+    }
+}
