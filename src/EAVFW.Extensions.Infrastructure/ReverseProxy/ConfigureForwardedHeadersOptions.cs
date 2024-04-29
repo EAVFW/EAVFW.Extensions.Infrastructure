9f26cd4735fd81959882122a9f4e96e508b38d72
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-04-29 16:56:58 +0200

    feat: added such known proxies is cleared if not provided in config

--- src/EAVFW.Extensions.Infrastructure/ReverseProxy/ConfigureForwardedHeadersOptions.cs
+++ src/EAVFW.Extensions.Infrastructure/ReverseProxy/ConfigureForwardedHeadersOptions.cs
@@ -1,4 +1,4 @@
-﻿
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
@@ -8,15 +8,25 @@ using System.Net;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    
     public static class ConfigureForwardedHeadersOptionsExtensions
     {
-        public static IServiceCollection ConfigureForwardedHeadersOptions(this IServiceCollection services,string setting= "ForwardedHeaders:KnownProxies")
+        public static IServiceCollection ConfigureForwardedHeadersOptions(this IServiceCollection services,string knownProxiesSettings= "ForwardedHeaders:KnownProxies")
         {
             services.AddOptions<ForwardedHeadersOptions>().Configure<IConfiguration>((options, configuration) =>
            {
-               foreach (var proxie in configuration.GetSection(setting).Get<string[]>()
-                   ?.Select(IPAddress.Parse) ?? Array.Empty<IPAddress>())
+               var knownProxies = configuration.GetSection(knownProxiesSettings).Get<string[]>()
+                   ?.Select(IPAddress.Parse);
+
+               foreach (var proxie in knownProxies ?? Array.Empty<IPAddress>())
                    options.KnownProxies.Add(proxie);
+
+               if (!knownProxies.Any())
+               {
+                   options.KnownProxies.Clear();
+                    
+               }
+               
 
                options.ForwardedHeaders =
                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost;
